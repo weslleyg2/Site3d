58be96b3af6fdb5a07943f9686bc69e1abb77985
Weslley Vander <90974762+weslleyg2@users.noreply.github.com>
2022-07-11 14:13:27 -0300

    modificações no multi-idioma

--- assets/js/multi_language.js
+++ assets/js/multi_language.js
@@ -1,111 +1,125 @@
 (function () {
     'use strict';
+    let $portuguese = document.querySelector(".br");
+    let $english = document.querySelector(".en");
 
-    // Cabeçalho
-    $(".br").click(function () {
-        $(".nav-menu").children().eq(0).text("Inicio");
-        $(".nav-menu").children().eq(1).text("Sobre");
-        $(".nav-menu").children().eq(2).text("Conhecimentos");
-        $(".nav-menu").children().eq(3).text("Projetos");
-        $(".nav-menu").children().eq(4).text("Certificados");
-        $(".nav-menu").children().eq(5).text("Contatos");
-        $(".language-selected").text("Português(Brasil)");
-        $(".language-selected").removeClass("change-en");
-        $(".language-selected").addClass("change-br");
+    $portuguese.addEventListener("click", portuguese);
+    $english.addEventListener("click", english);
+
+    function portuguese() {
+        // Cabeçalho
+        document.querySelector(".home").textContent = "Inicio";
+        document.querySelector(".about").textContent = "Sobre";
+        document.querySelector(".knowledge").textContent = "Conhecimentos";
+        document.querySelector(".projects").textContent = "Projetos";
+        document.querySelector(".certificates").textContent = "Certificados";
+        document.querySelector(".contact").textContent = "Contatos";
+        document.querySelector(".language-selected").textContent = "Português";
+        document.querySelector(".language-selected").classList.add("change-br");
+        document.querySelector(".language-selected").classList.remove("change-en");
+
         // Seção
-        $("#title").text("Olá, Meu Nome é Weslley Vander");
-        $("#sub-titile").text("Desenvolvedor Front-end.");
-        $("section p").text("Gosto de desenvolver ,e ajudar as pessoas com  minhas criações.");
-        $(".button-strong ").text("Conheça meu portifolio");
+        document.querySelector("#title").textContent = "Olá, Meu nome é Weslley Vander.";
+        document.querySelector("#sub-title").innerHTML = "Desenvolvedor Front-end.<span class='efect'>|</span>";
+        document.querySelector(".front-home p").textContent = "Gosto de desenvolver ,e ajudar as pessoas com  minhas criações.";
+        document.querySelector(".front-home .button").textContent = "Conheça meu portifolio";
         // Sobre
-        $(".about h2").text("Sobre");
-        $(".about p:nth-child(3)").text("Meu nome é Weslley Vander, tenho 26 anos, sou desenvolvedor front-end");
-        $(".about p:nth-child(4)").text("Atualmente estou estudando e desenvolvendo projetos web mais precisamente no Front-end, de modo a ampliar meus conhecimentos e assim aprimorar ainda mais as habilidades que possuo.");
-        $(".download .button-strong:nth-child(1)").text("Currículo em Português");
-        $(".download .button-strong:nth-child(2)").text("Currículo em Inglês");
+        document.querySelector(".about h2").textContent = "Sobre";
+        document.querySelector(".about p:nth-child(3)").textContent = "Meu nome é Weslley Vander, tenho 26 anos, sou desenvolvedor front-end";
+        document.querySelector(".about p:nth-child(4)").textContent = "Atualmente estou estudando e desenvolvendo projetos web mais precisamente no Front-end, de modo a ampliar meus conhecimentos e assim aprimorar ainda mais as habilidades que possuo.";
+        document.querySelector(".download .button-strong .button:nth-child(1)").textContent = "Currículo em Protuguês";
+        document.querySelector(".download .button-strong .button2").textContent = "Currículo em Inglês";
+       
         // Conhecimentos
 
-        $(".knowledge h2").text("Conhecimentos");
-        $(".knowledge .html h2").text("Html");
-        $(".knowledge .html h4").text("6 meses de experiência");
-        $(".knowledge .html p").text("HTML é a linguagem de marcação padrão para páginas da Web. Com HTML você pode criar seu próprio site.");
-        $(".knowledge .css h2").text("CSS");
-        $(".knowledge .css h4").text("6 meses de experiência");
-        $(".knowledge .css p").text("CSS é a linguagem que usamos para estilizar um documento HTML. CSS descreve como os elementos HTML devem ser exibido.");
-        $(".knowledge .javascript h2").text("Javascript");
-        $(".knowledge .javascript h4").text("2 meses de experiência");
-        $(".knowledge .javascript p").text("JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.");
+        document.querySelector(".knowledge .gradient").textContent = "Conhecimentos";
+        //Html
+        document.querySelector(".knowledge .html h2").textContent = "Html";
+        document.querySelector(".knowledge .html h4").textContent = "6 meses de experiência";
+        document.querySelector(".knowledge .html p").textContent = "HTML é a linguagem de marcação padrão para páginas da Web. Com HTML você pode criar seu próprio site.";
+        // Css
+        document.querySelector(".knowledge .css h2").textContent = "CSS";
+        document.querySelector(".knowledge .css h4").textContent = "6 meses de experiência";
+        document.querySelector(".knowledge .css p").textContent = "CSS é a linguagem que usamos para estilizar um documento HTML. CSS descreve como os elementos HTML devem ser exibido.";
+        //JavaScrip
+        document.querySelector(".knowledge .javascript h2").textContent = "Javascript";
+        document.querySelector(".knowledge .javascript h4").textContent = "2 meses de experiência";
+        document.querySelector(".knowledge .javascript p").textContent = "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.";
+        // Certificados
 
-        // certificados
-
-        $(".certificates h2").text("Certificados");
-        $(".certificates .web-design p").text("Curso Web Design Completo HTML5- CSS3 e JS mais  5 Pojetos");
-        $(".certificates .scrum p").text("Projetos ágeis com SCRUM");
-
+        document.querySelector(".certificates h2").textContent = "Certificados";
+        document.querySelector(".certificates .web-design p").textContent = "Certificado Curso Web Design Completo HTML5- CSS3 e JS mais  5 Pojetos";
+        document.querySelector(".certificates .scrum p").textContent = "Projetos ágeis com SCRUM";
         // Contato
         //email
-        $(".contacts h2").text("Contato");
-        $(".contacts .email h3").text("Envie um E-mail");
+
+        document.querySelector(".contacts h2").textContent = "Contato";
+        document.querySelector(".contacts .email h3").textContent = "Envie um E-mail";
         // linkedin
-        $(".contacts .email h3").text("Linkedin");
-        $(".contacts .email p").text("/weslley-vander-6a579392/");
-
+        
+        document.querySelector(".contacts .linkedin h3").textContent = "Linkedin";
+        document.querySelector(".contacts .linkedin p").textContent = "/weslley-vander-6a579392/";
         // rodapé
-        $(".footer span").text("© - 2022 por Weslley Vander - Desenvolvedor Front-end");
-    });
-        // ENGLISH
-        $(".en").click(function () {
-            $(".nav-menu").children().eq(0).text("Home");
-            $(".nav-menu").children().eq(1).text("About");
-            $(".nav-menu").children().eq(2).text("Knowledge");
-            $(".nav-menu").children().eq(3).text("Projects");
-            $(".nav-menu").children().eq(4).text("Certificates");
-            $(".nav-menu").children().eq(5).text("Contact");
-            $(".language-selected").text("English");
-            $(".language-selected").removeClass("change-br");
-            $(".language-selected").addClass("change-en");
-            // section
-            $("#title").text("Hello, My name is Weslley Vander");
-            $("#sub-titile").text("Front-end Developer.");
-            $("section p").text("I like to develop and help people with my creations.");
-            $(".button-strong ").text("Meet my portfolio");
+        document.querySelector(".footer span").innerHTML = "&copy; - 2022 por Weslley Vander - Desenvolvedor Front-end";
 
-            // About
-            $(".about h2").text("About");
-            $(".about p:nth-child(3)").text("My name is Weslley Vander, I'm 26 years old, I'm a front-end developer");
-            $(".about p:nth-child(4)").text("I'm currently studying and developing web projects more precisely on the Front-end, in order to expand my knowledge and thus improve even more the skills I have.");
-            $(".download .button-strong:nth-child(1)").text("Resume in Portuguese Brazil");
-            $(".download .button-strong:nth-child(2)").text("Resume in English");
-            // Knowledge
+    }
+    function english() {
+        // Cabeçalho
+        document.querySelector(".home").textContent = "Home";
+        document.querySelector(".about").textContent = "About";
+        document.querySelector(".knowledge").textContent = "Knowledge";
+        document.querySelector(".projects").textContent = "Projects";
+        document.querySelector(".certificates").textContent = "Certificates";
+        document.querySelector(".contact").textContent = "Contact";
+        document.querySelector(".language-selected").textContent = "English";
+        document.querySelector(".language-selected").classList.add("change-en");
+        document.querySelector(".language-selected").classList.remove("change-br");
 
-            $(".knowledge h2").text("Knowledge");
-            $(".knowledge .html h2").text("Html");
-            $(".knowledge .html h4").text("6 months of experience");
-            $(".knowledge .html p").text("HTML is the standard markup language for Web pages. With HTML you can create your own Website.");
+        // section
+        document.querySelector("#title").textContent = "Hello, My name is Weslley Vander";
+        document.querySelector("#sub-title").innerHTML = "Front-end Developer.<span class='efect'>|</span>";
+        document.querySelector(".front-home p").textContent = "I like to develop and help people with my creations.";
+        document.querySelector(".front-home .button").textContent = "Meet my portfolio";
+        // About
+        document.querySelector(".about h2").textContent = "About";
+        document.querySelector(".about p:nth-child(3)").textContent = "My name is Weslley Vander, I'm 26 years old, I'm a front-end developer";
+        document.querySelector(".about p:nth-child(4)").textContent = "I'm currently studying and developing web projects more precisely on the Front-end, in order to expand my knowledge and thus improve even more the skills I have.";
+        document.querySelector(".download .button-strong .button:nth-child(1)").textContent = "Resume in Portuguese";
+        document.querySelector(".download .button-strong .button2").textContent = "Resume in English";
+        
+        // Knowledge
 
-            $(".knowledge .css h2").text("CSS");
-            $(".knowledge .css h4").text("6 months of experience");
-            $(".knowledge .css p").text("CSS is the language we use to style an HTML document. CSS describes how HTML elements should be displayed.");
+        document.querySelector(".knowledge .gradient").textContent = "Knowledge";
+        //Html
+        document.querySelector(".knowledge .html h2").textContent = "Html";
+        document.querySelector(".knowledge .html h4").textContent = "6 months of experience";
+        document.querySelector(".knowledge .html p").textContent = "HTML is the standard markup language for Web pages. With HTML you can create your own Website.";
+        // Css
+        document.querySelector(".knowledge .css h2").textContent = "CSS";
+        document.querySelector(".knowledge .css h4").textContent = "6 months of experience";
+        document.querySelector(".knowledge .css p").textContent = "CSS is the language we use to style an HTML document. CSS describes how HTML elements should be displayed.";
+        //JavaScrip
+        document.querySelector(".knowledge .javascript h2").textContent = "Javascript";
+        document.querySelector(".knowledge .javascript h4").textContent = "2 months of experience";
+        document.querySelector(".knowledge .javascript p").textContent = "JavaScript is a structured, multi-paradigm, weak dynamically typed, high-level scripting interpreted programming language. Along with HTML and CSS, JavaScript is one of the three main technologies of the World Wide Web.";
+        // Certificates
 
-            $(".knowledge .javascript h2").text("Javascript");
-            $(".knowledge .javascript h4").text("2 months of experience");
-            $(".knowledge .javascript p").text("JavaScript is a structured, multi-paradigm, weak dynamically typed, high-level scripting interpreted programming language. Along with HTML and CSS, JavaScript is one of the three main technologies of the World Wide Web.");
+        document.querySelector(".certificates h2").textContent = "Certificates";
+        document.querySelector(".certificates .web-design p").textContent = "Complete Web Design Course HTML5- CSS3 and JS plus 5 Projects";
+        document.querySelector(".certificates .scrum p").textContent = "Agile projects with SCRUM";
+        // Contact
+        //email
 
-            // certificados
+        document.querySelector(".contacts h2").textContent = "Contact";
+        document.querySelector(".contacts .email h3").textContent = "Send an e-mail";
+        // linkedin
+        
+        document.querySelector(".contacts .linkedin h3").textContent = "Linkedin";
+        document.querySelector(".contacts .linkedin p").textContent = "/weslley-vander-6a579392/";
+        // rodapé
+        document.querySelector(".footer span").innerHTML = "&copy; - 2022 by Weslley Vander - Front-end Developer";
 
-            $(".certificates h2").text("Certificates");
-            $(".certificates .web-design p").text("Complete Web Design Course HTML5- CSS3 and JS plus 5 Projects");
-            $(".certificates .scrum p").text("Agile projects with SCRUM");
+    }
 
-            // Contact
-            //email
-            $(".contacts h2").text("Contact");
-            $(".contacts .email h3").text("Send an e-mail");
-            // linkedin
-            $(".contacts .email h3").text("Linkedin");
-            $(".contacts .email p").text("/weslley-vander-6a579392/");
 
-            // footer
-            $(".footer span").text("© - 2022 by Weslley Vander - Front-end Developer");
-        });
-    })();
+})();
